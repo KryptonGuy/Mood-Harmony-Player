31e99dd740152f4021905e9dd3864ff26cc5f78b
Ubuntu <azureuser@worker.a1vwqiowvheuhbq42wilgc0woe.rx.internal.cloudapp.net>
2023-11-24 14:09:24 +0000

    added the first

---
+++ root.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="Mood Harmony Player",
+    page_icon="🎧",
+    initial_sidebar_state="collapsed" 
+)
+
+
+st.write("Welcome to Mood Harmony Player! 👋")
